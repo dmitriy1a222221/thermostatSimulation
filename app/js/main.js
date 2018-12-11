8fd1edd514dc3ad03a1178ac5e4ff22939b8558b
dmitriy <dmitriy1a22222@gmail.com>
2018-12-12 00:44:18 +0200

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,9 @@
 
 let cooler = document.querySelector('.cooler');
-
-let flag = true;
-let collerActive = () => {
-    if(!flag) {
-        cooler.classList.remove('cooler-active');
-        flag = !flag
-        return
-    } else{
-        cooler.classList.add('cooler-active');
-        flag = !flag
-        return
-    }
-
-};
-//collerActive();
-let flag1 = true;
 let spiral = document.querySelector('.spiral');
 let rele1 = document.querySelector('.rele1');
 let rele2 = document.querySelector('.rele2');
 
-
-let spiralAcitve = () => {
-    if(!flag1) {
-        spiral.style.cssText = 'visibility: hidden; opacity: 0; z-index: -1';
-        flag1 = !flag1;
-        return
-    } else {
-        spiral.style.cssText = 'visibility: visible; opacity: 1; z-index: 2';
-        flag1 = !flag1;
-        return
-    }
-}
 
 let range = document.querySelectorAll('.nav-item-input');
 let spanCurrTemp = document.getElementById('currTemp');
@@ -41,18 +13,15 @@ range.forEach(item => {
         switch(item.dataset.id) {
             case 'temp':
                 spanCurrTemp.innerText = `${currTemp} deg`
-                console.log(currTemp)
+                thermostatLogic()
                 break
             case 'rele1':
                 rele1.innerText = `${currTemp} deg`
-                console.log(currTemp)
                 break
             case 'rele2':
                 rele2.innerText = `${currTemp} deg`
-                console.log(currTemp)
                 break
         }
-
     })
 })
 
@@ -62,24 +31,48 @@ let thermostatLogic = () => {
     let indicatorList = document.querySelectorAll('.indicator')
     let rele1State = document.querySelector('.rele1State')
     let rele2State = document.querySelector('.rele2State')
+    //
+    // tempInputList[1].setAttribute("disabled", "disabled")
+    // tempInputList[2].setAttribute("disabled", "disabled")
 
-    tempInputList[1].setAttribute("disabled", "disabled")
-    tempInputList[2].setAttribute("disabled", "disabled")
-
-    if (tempInputList[0].value >= tempInputList[1].value && tempInputList[0].value >= tempInputList[2].value) {
+    if (tempInputList[0].value >= tempInputList[1].value) {
         indicatorList[0].style.cssText = 'background-color: #ff0300'
         rele1State.innerText = 'On'
-        collerActive()
+        if(indicatorList[0].style.backgroundColor === 'rgb(255, 3, 0)') {
+            cooler.classList.add('cooler-active');
+        } else {
+            cooler.classList.remove('cooler-active')
+        }
         indicatorList[1].style.cssText = 'background-color: #ff0300'
         rele2State.innerText = 'On'
-        spiralAcitve()
     } else {
         indicatorList[0].style.cssText = 'background-color: #000000'
         rele1State.innerText = 'Off'
-        collerActive()
+        if(indicatorList[0].style.backgroundColor === 'rgb(255, 3, 0)') {
+            cooler.classList.add('cooler-active');
+        } else {
+            cooler.classList.remove('cooler-active')
+        }
         indicatorList[1].style.cssText = 'background-color: #000000'
         rele2State.innerText = 'Off'
-        spiralAcitve()
+    }
+    if (tempInputList[0].value >= tempInputList[2].value) {
+        indicatorList[1].style.cssText = 'background-color: #ff0300'
+        rele2State.innerText = 'On'
+        if(indicatorList[1].style.backgroundColor === 'rgb(255, 3, 0)') {
+            spiral.style.cssText = 'visibility: visible; opacity: 1; z-index: 2';
+        } else {
+            spiral.style.cssText = 'visibility: hidden; opacity: 0; z-index: -1';
+        }
+    } else {
+        indicatorList[1].style.cssText = 'background-color: #000000'
+        rele2State.innerText = 'Off'
+        if(indicatorList[1].style.backgroundColor === 'rgb(255, 3, 0)') {
+            spiral.style.cssText = 'visibility: visible; opacity: 1; z-index: 2';
+        } else {
+            spiral.style.cssText = 'visibility: hidden; opacity: 0; z-index: -1';
+        }
+
     }
 
 }
